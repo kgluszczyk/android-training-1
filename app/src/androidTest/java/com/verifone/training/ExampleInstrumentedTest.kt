26b92ce1bb4aa40ae519955a014f90dba66698db
Krzysztof Gluszczyk <gluszczykk@ryanair.com>
2019-10-11 22:18:26 +0200

    Training#1
    * fix package name

--- app/src/androidTest/java/com/verifone/training/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/verifone/training/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.ryanair.training", appContext.packageName)
+        assertEquals("com.verifone.training", appContext.packageName)
     }
 }
